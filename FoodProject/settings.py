bb0bfbf1fd060843b5c83c0ce32a06eb2950b00f
Suleman <sulemanpatras2@gmail.com>
2024-03-21 18:11:29 +0500

    Hosting

--- FoodProject/settings.py
+++ FoodProject/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -135,3 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
---
+++ build_files.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt 
+python3.10.12 manage.py collectstatic
